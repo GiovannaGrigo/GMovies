b74831762209ba02494f939cca737ecb65ed3f39
GiovannaGrigo <giovanna1grigo@gmail.com>
2024-12-09 13:29:46 -0300

    Finalização da página Sobre, alterações em todas as págians

--- js/pesquisar.js
+++ js/pesquisar.js
@@ -1,6 +1,11 @@
+// Ao carregar a página executa as funções de buscar os dados
+document.addEventListener("DOMContentLoaded", async () => {
+    toggleLoading();
+});
 
 document.getElementById('pesquisar').addEventListener('submit', async function(event) {
     event.preventDefault();
+    toggleLoading();
     let texto = this.querySelector('input[type="text"]').value;
     // console.log(texto);
 
@@ -9,20 +14,34 @@ document.getElementById('pesquisar').addEventListener('submit', async function(e
         .then(res => res.json())
         .then(res => resultados = res.results)
         .catch(err => console.error(err));
-    // console.log(resultados);
+    //console.log(resultados);
 
     let qtdeResultados = document.querySelector("#qtdeResultados");
     let divResultados = document.querySelector("#resultado");
-    
+
     divResultados.innerHTML = '';
     if (resultados.length > 0) {
         qtdeResultados.innerHTML = `Resultados Encontrados (${resultados.length})`;
         resultados.forEach(resultado => {
-            let poster = resultado.poster_path ? `https://image.tmdb.orgt/t/p/original/${resultado.poster_path}` : 'img/no-poster.png';
-            divResultados.innerHTML += ``;
-        })
+            let poster = resultado.poster_path ?
+                `https://image.tmdb.org/t/p/original/${resultado.poster_path}` :
+                'img/no-poster.png';
+            divResultados.innerHTML += 
+                `<div class="col">
+                    <a href="detalhes.html?id=${resultado.id}&media=${resultado.media_type}" class="text-decoration-none">
+                        <div class="card bg-dark h-100">
+                            <img src="${poster}" class="card-img-top" style="height:400px">
+                            <div class="card-body text-white">
+                                <h5 class="card-title text-center">
+                                    ${resultado.title ?? resultado.name}
+                                </h5>
+                            </div>
+                        </div>
+                    </a>
+                </div>`;
+        });
     } else {
-        qtdeResultados.innerHTML = `Nenhum resultado Encontrado`;
+        qtdeResultados.innerHTML = `Nenhum Resultado Encontrado`;
     }
-
+    toggleLoading();
 })
--- js/site.js
+++ js/site.js
@@ -15,3 +15,22 @@ const options = {
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0M2U1MGUyM2RlODU0MTEzNzJmY2IwYWI3Yzg2MjIxOCIsIm5iZiI6MTczMjIwNzczMi44NTU0ODk3LCJzdWIiOiI2NzNjZGQ0MTYwYjdiM2JjOTRhMGQzZTgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Flyoh8xDoqYKZCGSk1bg4ZIZSLOzX3Ab6sVcsN2w2U4'
     }
 };
+
+
+// Formatação de Datas
+function formatarData(data) {
+    var d = new Date(data),
+        mes = '' + (d.getMonth() + 1),
+        dia = '' + d.getDate(),
+        ano = d.getFullYear();
+    mes = mes.length < 2 ? '0' + mes : mes;
+    dia = dia.length < 2 ? '0' + dia : dia;
+    return [dia, mes, ano].join('/');
+}
+
+
+// Exibir e esconder Loading
+function toggleLoading() {
+    let loader = document.querySelector('.loader');
+    loader.style.display = loader.style.display == 'none' ? 'block' : 'none';
+}
